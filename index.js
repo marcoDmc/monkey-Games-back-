ed50f79cfed6cf4494122a14812aa042ff0ef3da
marcos <marcodamasceno07@gmail.com>
2022-04-04 00:11:19 -0300

    :sparkles: created server configuration

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const db = require("./database/db");
+const router = require("./routes/router");
+const cors = require("cors");
+
 
 app.use(express.json());
+app.use(cors());
+app.use(router);
 
 app.listen(process.env.PORT, () =>
   console.log("conect a server sucessfully 🔥")
